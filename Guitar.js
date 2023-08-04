ec372a9d2d55bca6ddd181fef4b6fb42cce95227
Tommyhh04 <131246343+Tommyhh04@users.noreply.github.com>
2023-08-04 13:13:48 +0200

    Update Guitar.js

--- Guitar.js
+++ Guitar.js
@@ -13,44 +13,44 @@ addEventListener("click", m1SoundGuitar);
 //Plays a music note assigned to a key on the keyboard to play when the assigned key is pressed
 function playSoundGuitar(event) {
   if (event.key === "1") {
-    let mySound = new Audio("sounds/chord-E.wav");
+    let mySound = new Audio(".sounds/chord-E.wav");
     mySound.play();
   } else if (event.key === "2") {
-    let mySound = new Audio("sounds/chord-A.wav");
+    let mySound = new Audio(".sounds/chord-A.wav");
     mySound.play();
   } else if (event.key === "3") {
-    let mySound = new Audio("sounds/chord-D.wav");
+    let mySound = new Audio(".sounds/chord-D.wav");
     mySound.play();
   } else if (event.key === "4") {
-    let mySound = new Audio("sounds/chord-G.wav");
+    let mySound = new Audio(".sounds/chord-G.wav");
     mySound.play();
   } else if (event.key === "5") {
-    let mySound = new Audio("sounds/chord-A.wav");
+    let mySound = new Audio(".sounds/chord-A.wav");
     mySound.play();
   } else if (event.key === "6") {
-    let mySound = new Audio("sounds/chord-E.wav");
+    let mySound = new Audio(".sounds/chord-E.wav");
     mySound.play();
   }
 }
 //Plays a music note when mouse is clicked on the individual button in the guitar image
 function m1SoundGuitar(mPress) {
   if (mPress.target === button1) {
-    let mySound = new Audio("sounds/chord-E.wav");
+    let mySound = new Audio(".sounds/chord-E.wav");
     mySound.play();
   } else if (mPress.target === button2) {
-    let mySound = new Audio("sounds/chord-A.wav");
+    let mySound = new Audio(".sounds/chord-A.wav");
     mySound.play();
   } else if (mPress.target === button3) {
-    let mySound = new Audio("sounds/chord-D.wav");
+    let mySound = new Audio(".sounds/chord-D.wav");
     mySound.play();
   } else if (mPress.target === button4) {
-    let mySound = new Audio("sounds/chord-G.wav");
+    let mySound = new Audio(".sounds/chord-G.wav");
     mySound.play();
   } else if (mPress.target === button5) {
-    let mySound = new Audio("sounds/chord-A.wav");
+    let mySound = new Audio(".sounds/chord-A.wav");
     mySound.play();
   } else if (mPress.target === button6) {
-    let mySound = new Audio("sounds/chord-E.wav");
+    let mySound = new Audio(".sounds/chord-E.wav");
     mySound.play();
   }
 }
