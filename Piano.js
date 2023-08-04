0e0246b6503312198d20b2bd766a40b5b7639bd5
Tommyhh04 <131246343+Tommyhh04@users.noreply.github.com>
2023-08-04 13:17:22 +0200

    Update Piano.js

--- Piano.js
+++ Piano.js
@@ -11,26 +11,26 @@ addEventListener("click", m1SoundPiano);
 //Plays a music note assigned to a key on the keyboard to play when the assigned key is pressed
 function playSoundPiano(keypress) {
   if (keypress.key === "1") {
-    let mySound = new Audio(".sounds/piano1.mp3");
+    let mySound = new Audio("./sounds/piano1.mp3");
     mySound.play();
   } else if (keypress.key === "2") {
-    let mySound = new Audio(".sounds/piano2.mp3");
+    let mySound = new Audio("./sounds/piano2.mp3");
     mySound.play();
   } else if (keypress.key === "3") {
-    let mySound = new Audio(".sounds/piano3.mp3");
+    let mySound = new Audio("./sounds/piano3.mp3");
     mySound.play();
   }
 }
 //Plays a music note when mouse is clicked on the individual button in the piano image
 function m1SoundPiano(mPress) {
   if (mPress.target === pianoKey1) {
-    let mySound = new Audio(".sounds/piano1.mp3");
+    let mySound = new Audio("./sounds/piano1.mp3");
     mySound.play();
   } else if (mPress.target === pianoKey2) {
-    let mySound = new Audio(".sounds/piano2.mp3");
+    let mySound = new Audio("./sounds/piano2.mp3");
     mySound.play();
   } else if (mPress.target === pianoKey3) {
-    let mySound = new Audio(".sounds/piano3.mp3");
+    let mySound = new Audio("./sounds/piano3.mp3");
     mySound.play();
   }
 }
